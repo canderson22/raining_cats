6864c6b5c9a969225f2afcb4179a4215a5f12e4e
Charles Anderson <charlesandreanderson22@gmail.com>
2017-09-06 15:32:03 -0700

    added new levels and css props

--- js/app.js
+++ js/app.js
@@ -3,16 +3,39 @@ var $body = $('body');
 var game = {
     score: 0,
     gameHeader: $('#game-header'),
+    clouds: $('.clouds'),
+    namePlace: $('#players-name'),
+    scoreCard: $('#score'),
+    missCard: $('#missed'),
     missed: 0,
     level: 1,
+    players: [
+        {
+            name: 'Player 1',
+            score: 0,
+            missed: 0
+        },
+        {
+            name: 'Player 2',
+            score: 0,
+            missed: 0
+        }
+    ],
+    switchTurn: function() {
+        if (this.currentPlayer == players[0]) {
+            this.currentPlayer = players[1];
+        }
+    },
     lostCat: function(cat) {
         $(cat).remove();
         this.missed++;
+        this.missCard.text(this.missed);
         this.checkMissed();
     },
     catchCat: function(cat) {
         $(cat).stop().remove();
         this.score++;
+        this.scoreCard.text(this.score);
     },
     checkMissed: function() {
         if (this.missed > 3) {
@@ -34,7 +57,6 @@ var game = {
                 top: vert
             }, speed, function() {
                 that.lostCat(this);
-                console.log(that.missed)
             });
 
             $body.append($cat);
@@ -53,8 +75,6 @@ var game = {
             return 'three'
         } else if (this.level == 4) {
             return 'four'
-        } else if (this.level == 5) {
-            return 'final'
         } else {
             return 'complete'
         }
@@ -66,14 +86,8 @@ var game = {
         this['level_' + this.checkLevel()]();
     },
     startGame: function() {
+        ht
         this.gameHeader.text('Get ready to save some cats!!');
-        var that = this;
-        this.rainCats(3000);
-        $body.on('click', '.cat', function() {
-            that.catchCat(this);
-        });
-    },
-    level_one: function() {
         var that = this;
         this.rainCats(3000);
         $body.on('click', '.cat', function() {
@@ -82,11 +96,43 @@ var game = {
     },
     level_two: function() {
         this.gameHeader.text("Congrats! Get Ready for Level 2!");
+        this.clouds.css({
+            background: '-webkit-linear-gradient(top, #d6dee4 5%, #9cdeff 100%)'
+        })
         var that = this;
         this.rainCats(2500);
         $body.on('click', '.cat', function() {
             that.catchCat(this);
         });
+    },
+    level_three: function() {
+        this.gameHeader.text("Congrats! Get Ready for Level 3!");
+        this.clouds.css({
+            background: '-webkit-linear-gradient(top, rgb(201, 207, 212) 5%, rgb(69, 193, 255) 100%)'
+        })
+        var that = this;
+        this.rainCats(2000);
+        $body.on('click', '.cat', function() {
+            that.catchCat(this);
+        });
+    },
+    level_four: function() {
+        this.gameHeader.text("Congrats! Get Ready for Level 4!");
+        this.clouds.css({
+            background: '-webkit-linear-gradient(top, #707273 5%, #00689c 100%)'
+        });
+        this.gameHeader.css({
+            color: 'white',
+            textShadow: '1px 0px 0px black'
+        })
+        var that = this;
+        this.rainCats(1500);
+        $body.on('click', '.cat', function() {
+            that.catchCat(this);
+        }); 
+    },
+    level_complete: function() {
+
     }
 }
 
