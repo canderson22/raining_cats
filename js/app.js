7b3ad740de876c602dd4132f1f92828f975270c1
Charles Anderson <charlesandreanderson22@gmail.com>
2017-09-07 15:36:03 -0700

    fixed bugs

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 var $body = $('body');
-var $btn = $('.btn');
+var $form = $('.form');
 
 var generateGame = function() {
     var $header = $('<header>');
@@ -42,7 +42,7 @@ var game = {
     },
     catchCat: function(cat) {
         $(cat).stop().remove();
-        this.score = this.score + 1;
+        this.score = this.score + 3;
         $('#score').text(this.score);
     },
     rainCats: function(speed) {
@@ -95,25 +95,35 @@ var game = {
        
     },
     level_two: function() {
-        $('#game-header').text("Get Ready for Level 2!");
+        $('#game-header').text("It's starting to rain harder");
         $('.clouds').css({
             background: '-webkit-linear-gradient(top, #d6dee4 5%, #9cdeff 100%)'
-        })
+        });
+
         var that = this;
-        setTimeout(this.rainCats(2500), 5000);
+        setTimeout(function() {
+            that.rainCats(2500)
+        }, 5000)
       
     },
     level_three: function() {
-        $('#game-header').text("Get Ready for Level 3!");
+        $('#game-header').text("Wow! Its really coming down now");
         $('.clouds').css({
             background: '-webkit-linear-gradient(top, rgb(201, 207, 212) 5%, rgb(69, 193, 255) 100%)'
         })
+
+
         var that = this;
-        setTimeout(this.rainCats(2000), 10000);
+        setTimeout(function() {
+            that.rainCats(2000)
+        }, 5000)
        
     },
     level_four: function() {
-        $('#game-header').text("Get Ready for Level 4!");
+        $('#game-header').text("It's a cat hurricane!");
+        $('header').css({
+            background: '-webkit-linear-gradient(top, #859398 5%, #283048 100%)'
+        })
         $('.clouds').css({
             background: '-webkit-linear-gradient(top, #707273 5%, #00689c 100%)'
         });
@@ -121,8 +131,16 @@ var game = {
             color: 'white',
             textShadow: '1px 0px 0px black'
         })
+        $('.clouds').children().children().css({
+            color: 'white',
+            textShadow: '1px 0px 0px black'
+        })
+  
+
         var that = this;
-        setTimeout(this.rainCats(1500), 10000);
+        setTimeout(function() {
+            that.rainCats(1500)
+        }, 5000)
        
     },
     level_complete: function() {
@@ -137,33 +155,72 @@ var game = {
 
 
         if (this.players[0].played == true && this.players[1].played == true) {
-            this.checkWinner();
+            var winner = this.checkWinner();
+            $body.empty();
+            var $h1 = $('<h1>');
+            $h1.text('The Winner is ' + winner.name + ' with a score of ' + winner.total);
+            var $reset = $('<button>');
+            $reset.addClass('btn');
+            $reset.text('Play Again');
+            var that = this;
+            $reset.on('click', function() {
+                that.switchTurn();
+                that.currentPlayer.score = 0;
+                that.currentPlayer.missed = 0;
+                that.level = 1;
+                that.currentPlayer.total = 0;
+                that.currentPlayer.played = false;
+                that.players[1].score = 0;
+                that.players[1].missed = 0;
+                that.players[1].total = 0;
+                that.players[1].played = false;
+                $body.empty();
+                $body.css({
+                    cursor: '-webkit-grab'
+                })
+                generateGame();
+                game.startGame(); 
+            })
+            $body.append($h1, $reset);
         } else {
-            var $h1 = $('<h1>Player 2 get Ready</h1>');
-            var $h2 = $('<h2>Score to beat ' + this.players[0].total)
+            var $h1 = $('<h1>Player 2 get Ready<br><h1>Score to beat ' + this.players[0].total + '</h1>');
             $h1.addClass('h1');
             $body.append($h1);
             setTimeout(function() {
                 $body.empty();
                 generateGame();
                 game.startGame()
-            }, 6000)
+            }, 10000)
         }
     },
     checkWinner: function() {
-        alert('good')
+        if (this.players[0].total > this.players[1].total) {
+            return this.players[0]
+        } else {
+            return this.players[1]
+        }
     }
 }
 game.currentPlayer = game.players[0];
 
 
-$btn.on('click', function() {
-    $body.empty();
-    $body.css({
-        cursor: '-webkit-grab'
-    })
-    generateGame();
-    game.startGame();
+$form.on('submit', function(e) {
+    e.preventDefault();
+    var $player1 = $('#player1Name');
+    var $player2 = $('#player2Name');
+    if ($player1.val() == '' || $player2.val() == '') {
+        alert('Please enter both names.')
+    } else {
+        game.players[0].name = $player1.val();
+        game.players[1].name = $player2.val();
+        $body.empty();
+        $body.css({
+            cursor: '-webkit-grab'
+        })
+        generateGame();
+        game.startGame(); 
+    }
+    
 })
 
 
